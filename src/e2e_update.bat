cdd470a49527719abbdb5b02437bbfd3426b594e
Davide <davide.motta@luxottica.com>
2025-11-21 11:36:14 -0500

    Add stock script execution to e2e_update.bat and create stock.bat

--- src/e2e_update.bat
+++ src/e2e_update.bat
@@ -17,6 +17,9 @@ echo Sales script execution...
 set PYTHONPATH=\\luxapplp04\Share\Gruppo_Demand_Planning\02_NPI\12_Report\E2E NPI Monitoring Dashbaord\script
 "C:\Users\mottad\AppData\Local\miniconda3\envs\e2e_env\python.exe" -m src.sales.sales
 
+echo Stock script execution...
+set PYTHONPATH=\\luxapplp04\Share\Gruppo_Demand_Planning\02_NPI\12_Report\E2E NPI Monitoring Dashbaord\script
+"C:\Users\mottad\AppData\Local\miniconda3\envs\e2e_env\python.exe" -m src.stock.stock
 
 
 
